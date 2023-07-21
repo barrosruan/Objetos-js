05c05be0917ac530eff302df71e48b40f986d699
Ruan Silva <ruan.fox.2013@gmail.com>
2023-07-20 21:03:14 -0300

    conclusão

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,33 @@
-export class ContaCorrente {
+import { Cliente } from "./Cliente.js";
+
+export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
-    cliente;
+    _cliente;
+   _saldo = 0;
+    
 
 
-    _saldo = 0;
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -13,17 +37,20 @@ export class ContaCorrente {
     }
 
     depositar(valor){
-        if(valor <=0)
+        if(valor <= 0)
         {
             return;
-        }
-            this._saldo += valor;
-        }
+        } 
+        this._saldo += valor;           
+    }
 
-    transferir(valor, conta){
+    trannferir(valor, conta){
         
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-
+        
     }
 }
+
+
+    
--- index.js
+++ index.js
@@ -1,28 +1,17 @@
- import {Cliente} from "./Cliente.js"
- import {ContaCorrente} from "./ContaCorrente.js"
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
- const cliente1 = new Cliente();  
- cliente1.nome = "Ricardo";
- cliente1.cpf = 123456789;
- 
- 
- const cliente2 = new Cliente();
- cliente2.nome = "Ruan";
- cliente2.cpf = 987654321;
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
 
- const contaCorrenteRicardo = new ContaCorrente()
- contaCorrenteRicardo.agencia = 1001;
- contaCorrenteRicardo.cliente = cliente1;
- contaCorrenteRicardo.depositar(500);
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
- const conta2 = new ContaCorrente();
- conta2.cliente = cliente2;
- conta2.agencia = 1002;
+const conta2 = new ContaCorrente(102, cliente2);
 
- contaCorrenteRicardo.transferir(100, conta2);
+let valor = 200;
+contaCorrenteRicardo.trannferir(valor, conta2);
 
-
- 
- console.log(conta2);
- console.log(contaCorrenteRicardo);
+console.log(contaCorrenteRicardo);
